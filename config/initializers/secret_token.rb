c62fc5949da0d82b4afdc179901edc416ff315e6
Jarmo <jsi1992@gmail.com>
2012-03-31 18:02:38 +0300

    luotu pohja

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+AktivatorPlus::Application.config.secret_token = 'f5c8ca16506cc63f5bbdb0a0498b73eafd8bc5b2fa5abc8c9635ccf7969609a2706ceb6661e321ba89c0cd077749ffa69a495cd124da5fa27daa179801c5e0a8'
