97ea1037d5021c6a8237a3bf5c69514060318ad3
Jarmo <jsi1992@gmail.com>
2012-04-02 00:13:39 +0300

    toimii kysymyksen luontiin saakka kysymys ei saa nimelle arvoa

--- app/controllers/questions_controller.rb
+++ app/controllers/questions_controller.rb
@@ -1,21 +1,29 @@
 class QuestionsController < ApplicationController
-
   def create
     @course = Course.find(params[:course_id])
     @question = @course.questions.create(params[:course])
-    redirect_to course_questions(@course)
-    #redirect_to courses# course_questions(@post)
-    #debugger
+    redirect_to course_questions_path(@course)
+  #redirect_to courses# course_questions(@post)
+  #debugger
   end
-  
+
   def new
     @course = Course.find(params[:course_id])
-    @question = @course.questions.create(params[:course])
-   #debugger
-  end
+    @question = @course.questions.new
   
+  #redirect_to course_questions_pat
+  end
+
   def show
+    #tän pitäis näyttää vaan yksi kyssäri
     @course = Course.find(params[:course_id])
+  end
+
+  def index
+    #näyttää listauksen kaikista kyssäreistä
+    @course = Course.find(params[:course_id])
+    
+    @questions = @course.questions
   end
 
 end
