e219a861a91305798020024f9aca25d9e79671fa
Jarmo <jsi1992@gmail.com>
2012-04-09 15:33:36 +0300

    adding aswer_options for new question

--- app/controllers/questions_controller.rb
+++ app/controllers/questions_controller.rb
@@ -2,6 +2,16 @@ class QuestionsController < ApplicationController
   def create
     @course = Course.find(params[:course_id])
     @question = @course.questions.create(params[:question])
+    @answer_option1 = @question.answer_options.build :name => params[:answer_option1] unless params[:answer_option1].blank?
+    @answer_option2 = @question.answer_options.build :name => params[:answer_option2] unless params[:answer_option2].blank?
+    @answer_option3 = @question.answer_options.build :name => params[:answer_option3] unless params[:answer_option3].blank?
+    @answer_option4 = @question.answer_options.build :name => params[:answer_option4] unless params[:answer_option4].blank?
+    @answer_option5 = @question.answer_options.build :name => params[:answer_option5] unless params[:answer_option5].blank?
+    @answer_option6 = @question.answer_options.build :name => params[:answer_option6] unless params[:answer_option6].blank?
+    @answer_option7 = @question.answer_options.build :name => params[:answer_option7] unless params[:answer_option7].blank?
+    @answer_option8 = @question.answer_options.build :name => params[:answer_option8] unless params[:answer_option8].blank?
+    @answer_option9 = @question.answer_options.build :name => params[:answer_option9] unless params[:answer_option9].blank?
+    @answer_option10 = @question.answer_options.build :name => params[:answer_option10] unless params[:answer_option10].blank?
     
     redirect_to course_questions_path(@course)
   #redirect_to courses# course_questions(@post)
@@ -11,13 +21,24 @@ class QuestionsController < ApplicationController
   def new
     @course = Course.find(params[:course_id])
     @question = @course.questions.new
-  
+    @answer_option1 = @question.answer_options.new
+    @answer_option2 = @question.answer_options.new
+    @answer_option3 = @question.answer_options.new
+    @answer_option4 = @question.answer_options.new
+    @answer_option5 = @question.answer_options.new
+    @answer_option6 = @question.answer_options.new
+    @answer_option7 = @question.answer_options.new
+    @answer_option8 = @question.answer_options.new
+    @answer_option9 = @question.answer_options.new
+    @answer_option10 = @question.answer_options.new
+    
   #redirect_to course_questions_pat
   end
 
   def show
     #tän pitäis näyttää vaan yksi kyssäri
     @course = Course.find(params[:course_id])
+    
   end
 
   def index
