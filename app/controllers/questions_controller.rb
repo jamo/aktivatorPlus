79e2a3e9b99da6c3401067756b196d013188bfdc
Jarmo <jsi1992@gmail.com>
2012-04-09 16:55:30 +0300

    saatiin answer_optionit syntymään

--- app/controllers/questions_controller.rb
+++ app/controllers/questions_controller.rb
@@ -2,16 +2,29 @@ class QuestionsController < ApplicationController
   def create
     @course = Course.find(params[:course_id])
     @question = @course.questions.create(params[:question])
-    @answer_option1 = @question.answer_options.build :name => params[:answer_option1] unless params[:answer_option1].blank?
-    @answer_option2 = @question.answer_options.build :name => params[:answer_option2] unless params[:answer_option2].blank?
-    @answer_option3 = @question.answer_options.build :name => params[:answer_option3] unless params[:answer_option3].blank?
-    @answer_option4 = @question.answer_options.build :name => params[:answer_option4] unless params[:answer_option4].blank?
-    @answer_option5 = @question.answer_options.build :name => params[:answer_option5] unless params[:answer_option5].blank?
-    @answer_option6 = @question.answer_options.build :name => params[:answer_option6] unless params[:answer_option6].blank?
-    @answer_option7 = @question.answer_options.build :name => params[:answer_option7] unless params[:answer_option7].blank?
-    @answer_option8 = @question.answer_options.build :name => params[:answer_option8] unless params[:answer_option8].blank?
-    @answer_option9 = @question.answer_options.build :name => params[:answer_option9] unless params[:answer_option9].blank?
-    @answer_option10 = @question.answer_options.build :name => params[:answer_option10] unless params[:answer_option10].blank?
+    #debugger
+    @answer_option1 = @question.answer_options.build :title => params[:answer_option1] unless params[:answer_option1].blank?
+    @answer_option2 = @question.answer_options.build :title => params[:answer_option2] unless params[:answer_option2].blank?
+    @answer_option3 = @question.answer_options.build :title => params[:answer_option3] unless params[:answer_option3].blank?
+    @answer_option4 = @question.answer_options.build :title => params[:answer_option4] unless params[:answer_option4].blank?
+    @answer_option5 = @question.answer_options.build :title => params[:answer_option5] unless params[:answer_option5].blank?
+    @answer_option6 = @question.answer_options.build :title => params[:answer_option6] unless params[:answer_option6].blank?
+    @answer_option7 = @question.answer_options.build :title => params[:answer_option7] unless params[:answer_option7].blank?
+    @answer_option8 = @question.answer_options.build :title => params[:answer_option8] unless params[:answer_option8].blank?
+    @answer_option9 = @question.answer_options.build :title => params[:answer_option9] unless params[:answer_option9].blank?
+    @answer_option10 = @question.answer_options.build :title => params[:answer_option10] unless params[:answer_option10].blank?
+    debugger
+    
+    @answer_option1.save! if @answer_option1
+    @answer_option2.save! if @answer_option2
+    @answer_option3.save! if @answer_option3
+    @answer_option4.save! if @answer_option4
+    @answer_option5.save! if @answer_option5
+    @answer_option6.save! if @answer_option6
+    @answer_option7.save! if @answer_option7
+    @answer_option8.save! if @answer_option8
+    @answer_option9.save! if @answer_option9
+    @answer_option10.save! if @answer_option10
     
     redirect_to course_questions_path(@course)
   #redirect_to courses# course_questions(@post)
@@ -21,6 +34,7 @@ class QuestionsController < ApplicationController
   def new
     @course = Course.find(params[:course_id])
     @question = @course.questions.new
+    #debugger
     @answer_option1 = @question.answer_options.new
     @answer_option2 = @question.answer_options.new
     @answer_option3 = @question.answer_options.new
@@ -31,8 +45,7 @@ class QuestionsController < ApplicationController
     @answer_option8 = @question.answer_options.new
     @answer_option9 = @question.answer_options.new
     @answer_option10 = @question.answer_options.new
-    
-  #redirect_to course_questions_pat
+
   end
 
   def show
