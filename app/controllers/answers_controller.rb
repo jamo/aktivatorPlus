c97263ed6e5eed88d99c6639f0a2def2aac1e727
Jarmo <jsi1992@gmail.com>
2012-04-15 03:15:20 +0300

    Saatiin vastausopitoille listaus for_for:iin toimimaan ja vastausvaihtoehtojen laskeminen toimii oikein - JEE

--- app/controllers/answers_controller.rb
+++ app/controllers/answers_controller.rb
@@ -4,15 +4,23 @@ class AnswersController < ApplicationController
     #debugger
     @course = Course.find(params[:course_id])
     @question = @course.questions.find(params[:question_id])
+    #miltei turha?
     @answer = @question.answers.new(:course_id => params[:course_id], :question_id => params[:question_id])
     #debugger
 
+
+    # <li><%= q.title %> : <%= radio_button_tag 'answer_option_id', q.choice%> </li>
+    # vanha answers#new lista
   end
   
   def create
-    @answer_option1 = @question.answer.build(:choice => params[:choice], :question_id => @question_id, :course_id => @course_id)
-    
-    
+    #debugger
+    @course = Course.find(params[:course_id])
+    @question = @course.questions.find(params[:question_id])
+    @answer = @question.answers.build(:choice => 1, :question_id => @question_id, :course_id => @course_id, :answer_option_id => params[:answer_option_id])
+    @answer.save! if @answer
+#    debugger
+#    redirect_to  course_question_path(@course_id, @question_id)
     #unless params[:answer_option1].blank?
    
     
