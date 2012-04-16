f33ce976c90f0e988ed60a32d7b3e01ed3ca4557
Jarmo <jsi1992@gmail.com>
2012-04-17 01:13:17 +0300

    kirjautumisen jälkeen järkevään paikkaan

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -6,7 +6,7 @@ class SessionsController < ApplicationController
   def create
     if user = User.authenticate(params[:name], params[:password])
       session[:user_id] = user.id
-      redirect_to admin_url
+      redirect_to root_path
     else
       redirect_to login_url, :alert => "Invalid user/password combination"
     end
