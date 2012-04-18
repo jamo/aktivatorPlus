d6bd46077ac1212e0fae9ce14f1c2e3f44879d55
Jarmo <jsi1992@gmail.com>
2012-04-18 15:03:11 +0300

    debugger sanaa ei kannata pushata herukuun, se kaataa sivut :D

--- app/controllers/courses_controller.rb
+++ app/controllers/courses_controller.rb
@@ -4,8 +4,6 @@ class CoursesController < ApplicationController
   # GET /courses.json
   def index
     @courses = Course.all
-    debugger
-
     respond_to do |format|
       format.html # index.html.erb
       format.json { render json: @courses }
