eb8f100197021a7355845b3960ddf8641ad51db7
Jarmo Isotalo <jsi1992@gmail.com>
2012-05-27 00:55:28 +0300

    Navi widh won't change when expanding menus

---
+++ app/assets/javascripts/courses.js
@@ -0,0 +1,6 @@
+$(document).ready(function() {
+   // put all your jQuery goodness in here.
+    $('.listedCourses' ).mouseenter(function(){$(this).css('backgroundColor', '#E8E8E8')}).mouseleave(function(){$(this).css('backgroundColor', 'transparent')})
+    $('table').css( 'border-collapse','collapse')    
+    //end
+ });
