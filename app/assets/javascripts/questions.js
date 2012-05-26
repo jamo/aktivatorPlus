46267c0d32006b23ecb1cba4581949f0dc412068
Jarmo Isotalo <jsi1992@gmail.com>
2012-05-26 23:55:19 +0300

    some javascript

---
+++ app/assets/javascripts/questions.js
@@ -0,0 +1,10 @@
+$(document).ready(function() {
+   // put all your jQuery goodness in here.
+    $('.listedQuestion' ).mouseenter(function(){$(this).css('backgroundColor', '#E8E8E8')}).mouseleave(function(){$(this).css('backgroundColor', 'transparent')})
+    
+
+    //end
+ });
+
+
+
