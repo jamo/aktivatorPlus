c0ef9695f10d566d25ccaaa5281dfa9a40adae60
Jarmo Isotalo <jsi1992@gmail.com>
2012-05-27 14:23:13 +0300

    no cancan, using only device for autenfication and roles

---
+++ db/migrate/20120527112016_add_index_admin_to_users.rb
@@ -0,0 +1,6 @@
+class AddIndexAdminToUsers < ActiveRecord::Migration
+  def change
+    add_column :users, :admin, :boolean, :null => false, :default => false
+
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20120527102008) do
+ActiveRecord::Schema.define(:version => 20120527112016) do
 
   create_table "answer_options", :force => true do |t|
     t.string   "title"
@@ -68,8 +68,8 @@ ActiveRecord::Schema.define(:version => 20120527102008) do
   add_index "questions", ["course_id"], :name => "index_questions_on_course_id"
 
   create_table "users", :force => true do |t|
-    t.string   "email",                  :default => "", :null => false
-    t.string   "encrypted_password",     :default => "", :null => false
+    t.string   "email",                  :default => "",    :null => false
+    t.string   "encrypted_password",     :default => "",    :null => false
     t.string   "reset_password_token"
     t.datetime "reset_password_sent_at"
     t.datetime "remember_created_at"
@@ -78,8 +78,9 @@ ActiveRecord::Schema.define(:version => 20120527102008) do
     t.datetime "last_sign_in_at"
     t.string   "current_sign_in_ip"
     t.string   "last_sign_in_ip"
-    t.datetime "created_at",                             :null => false
-    t.datetime "updated_at",                             :null => false
+    t.datetime "created_at",                                :null => false
+    t.datetime "updated_at",                                :null => false
+    t.boolean  "admin",                  :default => false, :null => false
   end
 
   add_index "users", ["email"], :name => "index_users_on_email", :unique => true
