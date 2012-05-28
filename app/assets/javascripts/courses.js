509d3aad5a4c6d968518b7da4d84c8e82ed5d385
Jarmo Isotalo <jsi1992@gmail.com>
2012-05-28 23:43:38 +0300

    new users, some still to do with authorization

--- app/assets/javascripts/courses.js
+++ app/assets/javascripts/courses.js
@@ -2,7 +2,6 @@ $(document).ready(function() {
    // put all your jQuery goodness in here.
     $('.listedCourses' ).mouseenter(function(){$(this).css('backgroundColor', '#E8E8E8')}).mouseleave(function(){$(this).css('backgroundColor', 'transparent')})
     $('table').css( 'border-collapse','collapse')
-    //end
 
     /* This also has effect to question listings*/
     jQuery( function($) {
@@ -16,4 +15,6 @@ $(document).ready(function() {
             });
         });
     });
+    //end
+
 });
